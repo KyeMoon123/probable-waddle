39e0c130411d594e03d3580f8a627114b5d4db2d
KyeMoon123 <63133356+KyeMoon123@users.noreply.github.com>
2022-07-24 19:14:36 +1000

    Update test123.js

--- test123.js
+++ test123.js
@@ -1,19 +1,14 @@
-// LitElement and html are the basic required imports
-import { LitElement, html, css } from "lit";
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 
-// Create a class definition for your component and extend the LitElement base class
-class BasicSetup extends LitElement {
-  // The render callback renders your element's template. This should be a pure function,
-  // it should always return the same template given the same properties. It should not perform
-  // any side effects such as setting properties or manipulating the DOM. See the updated
-  // or first-updated examples if you need side effects.
+@customElement('simple-greeting')
+export class SimpleGreeting extends LitElement {
+  static styles = css`p { color: blue }`;
+
+  @property()
+  name = 'Somebody';
+
   render() {
-    // Return the template using the html template tag. lit-html will parse the template and
-    // create the DOM elements
-    return html` <div>Hello world!!</div> `;
+    return html`<p>Hello, ${this.name}!</p>`;
   }
 }
-
-// Register your element to custom elements registry, pass it a tag name and your class definition
-// The element name must always contain at least one dash
-customElements.define("basic-setup", BasicSetup);
