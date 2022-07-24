37eda53b3726a4f3e3703671c499838db8b2f44f
KyeMoon123 <63133356+KyeMoon123@users.noreply.github.com>
2022-07-24 19:35:39 +1000

    Update test123.js

--- test123.js
+++ test123.js
@@ -1,2 +1,13 @@
-<p>Hello</p>
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 
+@customElement('my-element')
+export class MyElement extends LitElement {
+// TODO: Add a reactive property
+
+  render() {
+    return html`
+      <p>TODO: Add an expression</p>
+    `;
+  }
+}
