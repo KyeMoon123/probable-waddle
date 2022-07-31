48e3c85a79c4c476468975bbad6540a24cc2b798
KyeMoon123 <63133356+KyeMoon123@users.noreply.github.com>
2022-07-31 20:07:37 +1000

    Update test123.js

--- test123.js
+++ test123.js
@@ -4,24 +4,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { html, css, LitElement } from 'https://cdn.skypack.dev/lit';
-import { customElement, property } from 'https://cdn.skypack.dev/lit/decorators.js';
-
+import { html, css, LitElement } from "./node_modules/lit@2.2.8/index.js";
+import { customElement, property } from "./node_modules/lit@2.2.8/decorators.js";
 let SimpleGreeting = class SimpleGreeting extends LitElement {
     constructor() {
         super(...arguments);
-        this.name = 'Somebody';
-        this.id = "0";
+        this.id = "1";
+    }
+    getUserMessage() {
+        if (this.id == "abc123") {
+            return html `${this.id}`;
+        }
+        else {
+            return html `Please log in <button>Login</button>`;
+        }
     }
     render() {
-        return html `<p>${this.id}!</p>`;
+        return html `<p>${this.getUserMessage()}</p>`;
     }
 };
-SimpleGreeting.styles = css `p { color: blue }`;
+SimpleGreeting.styles = css `
+    p {
+      color: blue;
+    }
+  `;
 __decorate([
     property()
-], SimpleGreeting.prototype, "name", void 0);
+], SimpleGreeting.prototype, "id", void 0);
 SimpleGreeting = __decorate([
-    customElement('simple-greeting')
+    customElement("simple-greeting")
 ], SimpleGreeting);
 export { SimpleGreeting };
